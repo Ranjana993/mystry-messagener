7af9df472e08b42dd809cea11ab4108eecc613ea
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-04-27 22:31:33 +0530

    complted sign-in

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +20,7 @@ export default function RootLayout({
         <body className={inter.className}>
           {children}
           <Toaster />
-          </body>
+        </body>
       </AuthProvider>
     </html>
   );
